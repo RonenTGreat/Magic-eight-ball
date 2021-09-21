80a7f97320bc42bb349b568dd7d7c3d51d561c1e
ronentgreat <ronenh53@gmail.com>
2021-09-21 19:28:54 +0000

    generic

---
+++ Magic 8 ball.py
@@ -0,0 +1,16 @@
+# This program simulates a magic 8 ball
+
+"""
+There will be 20 responses.
+There will be 10 positive answers, 5 negative answers and 5 vague responses.
+"""
+from random import choice
+from time import sleep
+
+print("Hi, this program will help you make decisions depending on what you ask. \n")
+print("Type your question or just think about the question in your head or speak it loud.")
+
+input("Enter your question or press enter if you thought or spoke it out loud. \n>>")
+print("Please wait...")
+sleep(3)
+
