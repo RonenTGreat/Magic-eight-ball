3da322f5e4ccd5595abba2089f493dd1a55ba1e5
ronentgreat <ronenh53@gmail.com>
2021-09-21 19:54:18 +0000

    generic eight 8 ball

--- Magic 8 ball.py
+++ Magic 8 ball.py
@@ -15,4 +15,5 @@ print("Please wait...")
 sleep(3)
 
 answers = ["It is certain", "It is decidedly so.", "With a doubt.", "Yes definitely.", "You may rely on it.",
-           "As I see it, yes", "Most likely.", "Outlook good.", "Yes", "Signs point to yes.", ""]
+           "As I see it, yes", "Most likely.", "Outlook good.", "Yes", "Signs point to yes.", "Reply hazy, try again.",
+           "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."]
