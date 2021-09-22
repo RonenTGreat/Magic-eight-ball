312a5fb70307ccd7a3d3360e2ad23ec717914f06
ronentgreat <ronenh53@gmail.com>
2021-09-22 21:07:39 +0000

    Maintain

--- Magic 8 ball.py
+++ Magic 8 ball.py
@@ -10,13 +10,18 @@ from time import sleep
 print("Hi, this program will help you make decisions depending on what you ask. \n")
 print("Type your question or just think about the question in your head or speak it loud.")
 
-input("Enter your question or press enter if you thought or spoke it out loud. \n>>")
-print("Please wait...")
-sleep(1)
+while True:
+    state = input("Enter your question or press enter if you thought or spoke it out loud. Enter \"q\" to  \n>>")
+    print("Please wait... \n")
+    sleep(1)
 
-answers = ["It is certain", "It is decidedly so.", "With a doubt.", "Yes definitely.", "You may rely on it.",
-           "As I see it, yes", "Most likely.", "Outlook good.", "Yes", "Signs point to yes.", "Reply hazy, try again.",
-           "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."
-           "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+    answers = ["It is certain.", "It is decidedly so.", "With a doubt.", "Yes definitely.", "You may rely on it.",
+               "As I see it, yes", "Most likely.", "Outlook good.", "Yes", "Signs point to yes.",
+               "Reply hazy, try again.",
+               "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."
+                                                                                      "Don't count on it.",
+               "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+    if state.lower() == "q":
+        break
 
-print(choice(answers))
+    print(choice(answers))
